b37268743cd08a821d0594765c70205d7a1b6356
MarianaDiasA <marianadiash123@gmail.com>
2024-04-18 19:39:59 -0300

    atividade5

---
+++ atividade5/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const estoque = require('./src/estoque');
+const app = express();
+
+app.get('/adicionar/:id/:nome/:qtd', function (req, res) {
+    let id = req.params.id;
+    let nome = req.params.nome;
+    let qtd = req.params.qtd;
+    let p = estoque.criarProduto(id, nome, qtd);
+    estoque.adicionarProduto(p);
+    res.send(p);
+});
+
+app.get('/listar', function (req, res) {
+    res.send(estoque.listarProdutos());
+})
+
+app.get('/remover/:id', function (req, res) {
+    let id = req.params.id;
+    estoque.removerProduto(id);
+    res.send(id);
+});
+
+app.get('/editar/:id/:qtd', function(req,res){
+    let id = req.params.id;
+    let qtd = req.params.qtd;
+    estoque.editarProduto(id,qtd);
+    res.send(qtd);
+});
+
+const PORT = 8080;
+app.listen(PORT, function () {
+    console.log('app rodando na porta ' + PORT);
+});
---
+++ atividade5/src/estoque.js
@@ -0,0 +1,40 @@
+let produtos = [];
+
+function criarProduto(id, nome, qtd) {
+    let p = {
+        id: id,
+        nome: nome,
+        qtd: qtd
+    }
+    return p;
+}
+
+function adicionarProduto(p) {
+    produtos.push(p);
+}
+
+function editarProduto(id, qtd) {
+    produtos = produtos.filter((p) =>{
+        if(p.id == id){
+            return p.qtd = qtd;
+        }
+    });
+}
+
+function listarProdutos() {
+    return produtos;
+}
+
+function removerProduto(id) {
+    produtos = produtos.filter((p) => {
+        let x = p.id != id;
+        return x;
+    });
+}
+
+module.exports = {
+    criarProduto,
+    adicionarProduto,
+    listarProdutos,
+    removerProduto
+};
